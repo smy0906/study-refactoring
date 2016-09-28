6ea70b1331a70533de4e93b0e44248c8c753f990
smy0906 <smy0906@gmail.com>
2016-09-28 14:30:06 +0900

    Ch1. Origin Version

---
+++ src/main/com/yang/refactoring/Main.java
@@ -0,0 +1,10 @@
+package main.com.yang.refactoring;
+
+/**
+ * Created by yang on 2016. 9. 28..
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
