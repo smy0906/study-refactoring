9bc2d0cb902930288cd4b5e6ec2ea17b6501cbac
smy0906 <smy0906@gmail.com>
2016-09-28 15:38:14 +0900

    Ch1. Revision 10 : set price code using price class

--- src/main/com/yang/refactoring/Movie.java
+++ src/main/com/yang/refactoring/Movie.java
@@ -10,7 +10,7 @@ public class Movie {
     public static final int NEW_RELEASE = 1;
 
     private String _title;
-    private int _priceCode;
+    private Price _price;
 
     public Movie(String title, int priceCode) {
         _title = title;
@@ -18,35 +18,27 @@ public class Movie {
     }
 
     public int getPriceCode() {
-        return _priceCode;
+        return _price.getPriceCode();
     }
 
     public void setPriceCode(int arg) {
-        _priceCode = arg;
+        switch (arg) {
+            case REGULAR:
+                _price = new RegularPrice();
+                break;
+            case CHILDRENS:
+                _price = new ChildrensPrice();
+                break;
+            case NEW_RELEASE:
+                _price = new NewReleasePrice();
+                break;
+            default:
+                throw new IllegalArgumentException("Price Code is wrong!");
+        }
     }
 
     public String getTitle() {
         return _title;
-    }
-
-    public double getCharge(int daysRented) {
-        double result = 0;
-        switch (getPriceCode()) {
-            case Movie.REGULAR:
-                result += 2;
-                if (daysRented > 2)
-                    result += (daysRented - 2) * 1.5;
-                break;
-            case Movie.NEW_RELEASE:
-                result += daysRented * 3;
-                break;
-            case Movie.CHILDRENS:
-                result += 1.5;
-                if (daysRented > 3)
-                    result += (daysRented - 3) * 1.5;
-                break;
-        }
-        return result;
     }
 
     public int getFrequntRenterPoints(int daysRented) {
